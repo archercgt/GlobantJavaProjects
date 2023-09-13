e968f3fe30c156c536e0853d27f95b2378125439
“Carlos <cartobon@gmail.com>
2023-09-13 18:03:13 -0500

    Add exercise 6 of Exceptions

---
+++ Excepciones/src/main/MetodoA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Archer
+ */
+public class MetodoA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        metodoA();
+    }
+
+    public static void metodoA() {
+        System.out.println("sentencia_a1");
+        System.out.println("sentencia_a2");
+        try {
+            System.out.println(Integer.parseInt("sentencia_a3"));
+            System.out.println(Integer.parseInt("sentencia_a4"));
+        } catch (Exception e) {
+            System.out.println("sentencia_a6");
+        }
+        System.out.println("sentencia_a5");
+    }
+}
