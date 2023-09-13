8537b78cfff6c5437e2f86c99b83645ef7399b41
“Carlos <cartobon@gmail.com>
2023-09-13 18:42:17 -0500

    Add classes for exercises 7 and 8

---
+++ Excepciones/src/main/ClaseTres.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Archer
+ */
+public class ClaseTres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            System.out.println(metodo());
+        } catch (Exception e) {
+            System.err.println("Excepcion en metodo() ");
+            e.printStackTrace();
+        }
+    }
+
+    private static int metodo() {
+        int valor = 0;
+        try {
+            valor = valor + 1;
+            valor = valor + Integer.parseInt("W");
+            valor = valor + 1;
+            System.out.println("Valor final del try : " + valor);
+        } catch (NumberFormatException e) {
+            valor = valor + Integer.parseInt("W");
+            System.out.println("Valor final del catch  : " + valor);
+        } finally {
+            valor = valor + 1;
+            System.out.println("Valor final del finally:" + valor);
+        }
+        valor = valor + 1;
+        System.out.println("Valor antes del return: " + valor);
+        return valor;
+    }
+
+}
---
+++ Excepciones/src/main/MetodoB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Archer
+ */
+public class MetodoB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        metodoB();
+    }
+
+    public static void metodoB() {
+        System.out.println("sentencia_b1");
+        try {
+            System.out.println(Integer.parseInt("sentencia_b2"));
+        } catch (Exception e) {
+            System.out.println("sentencia_b3");
+        } finally {
+            System.out.println("sentencia_b4");
+        }
+    }
+}
