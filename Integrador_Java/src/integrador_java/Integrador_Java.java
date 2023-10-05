6820847163a77649acca7b469da516547a2a1c81
“Carlos <cartobon@gmail.com>
2023-10-05 14:41:18 -0500

    A project was created for testing some question of the Java MySQL test

---
+++ Integrador_Java/src/integrador_java/Integrador_Java.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package integrador_java;
+
+/**
+ *
+ * @author Archer
+ */
+public class Integrador_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona p1 = new Persona();
+        String nombre = null;
+        if(!nombre.equals(null)){
+            p1.setNombre(nombre);
+        }
+    }
+    
+}
