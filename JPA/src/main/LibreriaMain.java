4f64c61c1394516f126f005741fc6cf3bcbfdd2f
“Carlos <cartobon@gmail.com>
2023-10-17 18:45:52 -0500

    Creation of the JPA project entities

--- JPA/src/main/LibreriaMain.java
+++ JPA/src/main/LibreriaMain.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jpa;
+package main;
 
 /**
  *
  * @author Archer
  */
-public class JPA {
+public class LibreriaMain {
 
     /**
      * @param args the command line arguments
