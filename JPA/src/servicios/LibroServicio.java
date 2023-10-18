416a4c33f627b5282cd4527e2599cc08c8f43a11
“Carlos <cartobon@gmail.com>
2023-10-17 19:07:21 -0500

    Add classes for entity services

---
+++ JPA/src/servicios/LibroServicio.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicios;
+
+/**
+ *
+ * @author Archer
+ */
+public class LibroServicio implements Funcionalidades{
+
+    public LibroServicio() {
+    }
+
+    @Override
+    public void consultar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void crear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
