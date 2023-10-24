06842f418516e4edb4be37414e7a3963662c6100
“Carlos <cartobon@gmail.com>
2023-10-23 19:06:09 -0500

    Creation of a menu for exercise 1 of JPA

--- JPA/src/main/LibreriaMain.java
+++ JPA/src/main/LibreriaMain.java
@@ -22,28 +22,88 @@ public class LibreriaMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
-        
+        boolean continuar = true;
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPAPU");
         EntityManager em = emf.createEntityManager();
-        
+
         AutorServicio as = new AutorServicio();
         EditorialServicio es = new EditorialServicio();
         LibroServicio ls = new LibroServicio();
-        
-        //as.crear(scanner, em);
-        //es.crear(scanner, em);
-        //System.out.println(as.consultar(scanner, em));
-        //System.out.println(es.consultar(scanner, em));
-        //ls.crear(scanner, em);
-        //es.modificar(scanner, em);
-        //System.out.println(es.consultar(scanner, em));
-        System.out.println(ls.consultar(scanner, em));
-        
-        
+
+        final String[] mensaje = {
+            "Bienvenido!!! Ingrese la opción que desea ejecutar:",
+            "1. Crear un autor",
+            "2. Crear una editorial",
+            "3. Crear un libro",
+            "4. Modificar un autor",
+            "5. Modificar una editorial",
+            "6. Modificar un libro",
+            "7. Consultar un autor",
+            "8. Consultar una editorial",
+            "9. Consultar un libro",
+            "10. Consultar libros",
+            "11. Salir"
+        };
+        while (continuar) {
+            for (String line : mensaje) {
+                System.out.println(line);
+            }
+            String opcion = validarInput(scanner);
+
+            switch (opcion) {
+                case "1":
+                    as.crear(scanner, em);
+                    break;
+                case "2":
+                    es.crear(scanner, em);
+                    break;
+                case "3":
+                    ls.crear(scanner, em);
+                    break;
+                case "4":
+                    as.modificar(scanner, em);
+                    break;
+                case "5":
+                    es.modificar(scanner, em);
+                    break;
+                case "6":
+                    ls.modificar(scanner, em);
+                    break;
+                case "7":
+                    System.out.println(as.consultar(scanner, em));
+                    System.out.println("");
+                    break;
+                case "8":
+                    System.out.println(es.consultar(scanner, em));
+                    System.out.println("");
+                    break;
+                case "9":
+                    System.out.println(ls.consultar(scanner, em));
+                    System.out.println("");
+                    break;
+                case "10":
+                    System.out.println(ls.consultarLibros(scanner, em));
+                    System.out.println("");
+                    break; 
+                case "11":
+                    System.out.println("Hasta luego, Vuelva pronto!!!");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("La opción ingresada es incorrecta");
+            }
+        }
         scanner.close();
     }
-    
+
+    public static String validarInput(Scanner scanner) throws Exception {
+        String input = scanner.next();
+        if (input == null || input.trim().isEmpty()) {
+            throw new Exception("El valor ingresar no puede estar vacío");
+        }
+        return input;
+    }
 }
--- JPA/src/servicios/LibroServicio.java
+++ JPA/src/servicios/LibroServicio.java
@@ -8,6 +8,8 @@ package servicios;
 import entidades.Autor;
 import entidades.Editorial;
 import entidades.Libro;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
@@ -16,12 +18,12 @@ import javax.persistence.NoResultException;
  *
  * @author Archer
  */
-public class LibroServicio extends Servicio{
+public class LibroServicio extends Servicio {
 
     public LibroServicio() {
     }
 
-    public Libro consultar(Scanner scanner, EntityManager em) throws Exception{
+    public Libro consultar(Scanner scanner, EntityManager em) throws Exception {
         final String[] mensaje = {
             "Indique la opción según el parametro que desea utilizar para la búsqueda del libro;",
             "1. Buscar libro por ISBN",
@@ -32,7 +34,7 @@ public class LibroServicio extends Servicio{
         }
         try {
             String opcion = validarInput(scanner);
-            Libro libro= null;
+            Libro libro = null;
             switch (opcion) {
                 case "1":
                     System.out.println("Ingrese el ISBN del libro");
@@ -62,10 +64,57 @@ public class LibroServicio extends Servicio{
             throw new Exception("Error: Libro no encontrado!!!");
         } catch (Exception e) {
             throw new Exception("Error durante la consulta");
-        }       
+        }
     }
 
-    public void crear(Scanner scanner, EntityManager em) throws Exception{
+    public Collection<Libro> consultarLibros(Scanner scanner, EntityManager em) throws Exception {
+        final String[] mensaje = {
+            "Indique la opción según el parametro que desea utilizar para la búsqueda de los libros;",
+            "1. Buscar libros por autor",
+            "2. Buscar libros por editorial"
+        };
+        for (String line : mensaje) {
+            System.out.println(line);
+        }
+        try {
+            String opcion = validarInput(scanner);
+            String nombre;
+            Collection<Libro> libros = null;
+            switch (opcion) {
+                case "1":
+                    System.out.println("Ingrese el nombre del autor");
+                    nombre = validarInput(scanner);
+                    libros = em.createQuery("SELECT l"
+                            + " FROM Libro l"
+                            + " JOIN l.autor a"
+                            + " WHERE a.nombre = :nombre").
+                            setParameter("nombre", nombre).
+                            getResultList();
+                    break;
+                case "2":
+                    System.out.println("Ingrese el nombre de la editorial");
+                    nombre = validarInput(scanner);
+                    libros = em.createQuery("SELECT l"
+                            + " FROM Libro l"
+                            + " JOIN l.editorial e"
+                            + " WHERE e.nombre = :nombre").
+                            setParameter("nombre", nombre).
+                            getResultList();
+                    break;
+                default:
+                    System.out.println("La opción ingresada no es valida.");
+            }
+            return libros;
+        } catch (NumberFormatException e) {
+            throw new Exception("Error: El valor ingresado debe ser un número!!!");
+        } catch (NoResultException e) {
+            throw new Exception("Error: No se encontraron libros!!!");
+        } catch (Exception e) {
+            throw new Exception("Error durante la consulta");
+        }
+    }
+
+    public void crear(Scanner scanner, EntityManager em) throws Exception {
         Libro libro = new Libro();
         AutorServicio as = new AutorServicio();
         EditorialServicio es = new EditorialServicio();
@@ -99,16 +148,77 @@ public class LibroServicio extends Servicio{
             System.out.println("");
         } catch (NumberFormatException e) {
             throw new Exception("Error: El valor ingresado debe ser un número!!!");
-        } catch (Exception e){
+        } catch (Exception e) {
             throw e;
         }
     }
 
-    public void modificar(Scanner scanner, EntityManager em) throws Exception{
-   
+    public void modificar(Scanner scanner, EntityManager em) throws Exception {
+        Libro libro = consultar(scanner, em);
+        final String[] mensaje = {
+            "Indique la opción según la modificación que desee hace sobre la editorial;",
+            "1. Dar de alta/baja",
+            "2. Modificar el título",
+            "3. Modificar la cantidad de ejemplares",
+            "4. Modificar el año",
+            "5. Modificar el autor",
+            "6. Modificar la editorial"
+        };
+        for (String line : mensaje) {
+            System.out.println(line);
+        }
+        String opcion = validarInput(scanner);
+        switch (opcion) {
+            case "1":
+                System.out.println("Seleccione según desee dar de alta o de baja el libro");
+                System.out.println("1. Alta");
+                System.out.println("2. Baja");
+                String opcion2 = validarInput(scanner);
+                switch (opcion2) {
+                    case "1":
+                        libro.setAlta(true);
+                        break;
+                    case "2":
+                        libro.setAlta(false);
+                        break;
+                }
+                break;
+            case "2":
+                System.out.println("Ingrese el nuevo título que tendrá el libro");
+                String titulo = validarInput(scanner);
+                libro.setTitulo(titulo);
+                break;
+            case "3":
+                System.out.println("Ingrese la nueva cantidad de ejemplares");
+                int ejemplares = Integer.parseInt(validarInput(scanner));
+                libro.setEjemplares(ejemplares);
+                break;
+            case "4":
+                System.out.println("Ingrese el nuevo año");
+                int anio = Integer.parseInt(validarInput(scanner));
+                libro.setEjemplares(anio);
+                break;
+            case "5":
+                AutorServicio as = new AutorServicio();
+                Autor autor = as.consultar(scanner, em);
+                libro.setAutor(autor);
+                break;
+            case "6":
+                EditorialServicio es = new EditorialServicio();
+                Editorial editorial = es.consultar(scanner, em);
+                libro.setEditorial(editorial);
+                break;
+            default:
+                System.out.println("La opción ingresada es invalida");
+        }
+        em.getTransaction().begin();
+        em.merge(libro);
+        em.getTransaction().commit();
+        System.out.println("Libro modificado con éxito");
+        System.out.println("");
     }
 
     public void eliminar(Scanner scanner) {
-        
+
     }
 }
