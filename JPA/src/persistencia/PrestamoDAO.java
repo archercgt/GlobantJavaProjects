e74df3f4155f4fac4b2cadef622faba482dcddd2
“Carlos <cartobon@gmail.com>
2023-10-29 20:10:55 -0500

    Update Cliente creation method

--- JPA/src/persistencia/PrestamoDAO.java
+++ JPA/src/persistencia/PrestamoDAO.java
@@ -5,10 +5,47 @@
  */
 package persistencia;
 
+import entidades.Prestamo;
+import java.util.List;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author Archer
  */
-public class PrestamoDAO {
+public class PrestamoDAO extends DAO<Prestamo> {
+
+    public boolean validacionPrestamo(Prestamo prestamo) {
+        // Define a JPQL query to check for the existence of a Prestamo with the same attributes
+        String jpql = "SELECT p FROM Prestamo p WHERE p.fechaPrestamo = :fechaPrestamo "
+                + "AND p.fechaDevolucion = :fechaDevolucion "
+                + "AND p.libro = :libro "
+                + "AND p.cliente = :cliente";
+
+        // Create a TypedQuery from the JPQL query
+        TypedQuery<Prestamo> query = em.createQuery(jpql, Prestamo.class);
+
+        // Set parameters for the query
+        query.setParameter("fechaPrestamo", prestamo.getFechaPrestamo());
+        query.setParameter("fechaDevolucion", prestamo.getFechaDevolucion());
+        query.setParameter("libro", prestamo.getLibro());
+        query.setParameter("cliente", prestamo.getCliente());
+
+        // Execute the query and retrieve the results
+        List<Prestamo> resultList = query.getResultList();
+
+        // Check if any matching Prestamo objects exist
+        return !resultList.isEmpty();
+    }
     
+    @Override
+    public void guardar(Prestamo prestamo) {
+        super.guardar(prestamo);
+    }
+
+    @Override
+    public void editar(Prestamo prestamo) {
+        super.editar(prestamo);
+    }
+
 }
--- JPA/src/servicios/PrestamoServicio.java
+++ JPA/src/servicios/PrestamoServicio.java
@@ -13,87 +13,73 @@ import java.util.List;
 import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import persistencia.ClienteDAO;
+import persistencia.LibroDAO;
+import persistencia.PrestamoDAO;
 
 /**
  *
  * @author Archer
  */
 public class PrestamoServicio extends Servicio {
-//
-//    public PrestamoServicio() {
-//    }
-//
+
+    ClienteDAO clienteDAO = new ClienteDAO();
+    PrestamoDAO prestamoDAO = new PrestamoDAO();
+    LibroDAO libroDAO = new LibroDAO();
+    ClienteServicio cs = new ClienteServicio();
+    LibroServicio ls = new LibroServicio();
+    
+
+    @Override
+    public void crear(Scanner scanner) throws Exception {
+        Prestamo prestamo = new Prestamo();
+        try {
+            System.out.println("Ingrese la fecha de prestamo en el formato YYYY-MM-DD");
+            String f_prestamo_str = validarInput(scanner);
+            Date f_prestamo = Date.valueOf(f_prestamo_str);
+            System.out.println("Ingrese la fecha de devolución en el formato YYYY-MM-DD");
+            String f_devolucion_str = validarInput(scanner);
+            Date f_devolucion = Date.valueOf(f_devolucion_str);
+            if (f_prestamo.after(f_devolucion)) {
+                throw new Exception("La fecha de prestamo no puede ser menor a la fecha de devolución");
+            }
+            prestamo.setFechaPrestamo(f_prestamo);
+            prestamo.setFechaDevolucion(f_devolucion);
+            System.out.println("Ingrese el id del cliente que se asignará al préstamo");
+            int id = Integer.valueOf(validarInput(scanner));
+            Cliente cliente = clienteDAO.buscarPorId(id);
+            prestamo.setCliente(cliente);
+            System.out.println("Ingrese el ISBN del libro que se asignará al préstamo");
+            Long isbn = Long.parseLong(validarInput(scanner));
+            Libro libro = libroDAO.buscarPorIsbn(isbn);
+            if (libro.getEjemplaresRestantes() > 0) {
+                prestamo.setLibro(libro);
+                if (prestamoDAO.validacionPrestamo(prestamo)) {
+                    throw new Exception("Error: el préstamo ya existe");
+                }
+                libro.setEjemplaresPrestados(libro.getEjemplaresPrestados() + 1);
+                libro.setEjemplaresRestantes(libro.getEjemplaresRestantes() - 1);
+                libroDAO.editar(libro);
+                prestamoDAO.guardar(prestamo);
+            } else {
+                throw new Exception("Error: no se puede realizar el prestámo ya que no hay ejemplares disponibles");
+            }
+        } catch (IllegalArgumentException e) {
+            throw new Exception("Error: La fecha debe estar en el formato YYYY-MM-DD");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
 //    public Prestamo consultar(Scanner scanner, EntityManager em) throws Exception {
 //        Prestamo prestamo = new Prestamo();
 //        return prestamo;
 //    }
 //
-//    public void crear(Scanner scanner, EntityManager em) throws Exception {
-//        Prestamo prestamo = new Prestamo();
-//        ClienteServicio cs = new ClienteServicio();
-//        LibroServicio ls = new LibroServicio();
-//        try {
-//            System.out.println("Ingrese la fecha de prestamo en el formato YYYY-MM-DD");
-//            String f_prestamo_str = validarInput(scanner);
-//            Date f_prestamo = Date.valueOf(f_prestamo_str);
-//            System.out.println("Ingrese la fecha de devolución en el formato YYYY-MM-DD");
-//            String f_devolucion_str = validarInput(scanner);
-//            Date f_devolucion = Date.valueOf(f_devolucion_str);
-//            if (f_prestamo.after(f_devolucion)) {
-//                throw new Exception("La fecha de prestamo no puede ser menor a la fecha de devolución");
-//            }
-//            prestamo.setFechaPrestamo(f_prestamo);
-//            prestamo.setFechaDevolucion(f_devolucion);
-//            Cliente cliente = cs.consultar(scanner, em);
-//            prestamo.setCliente(cliente);
-//            Libro libro = ls.consultar(scanner, em);
-//            if (libro.getEjemplaresRestantes() > 0) {
-//                prestamo.setLibro(libro);
-//                if(validacionPrestamo(prestamo, em)){
-//                    throw new Exception("Error: el préstamo ya existe");
-//                }
-//                libro.setEjemplaresPrestados(libro.getEjemplaresPrestados() + 1);
-//                libro.setEjemplaresRestantes(libro.getEjemplaresRestantes() - 1);
-//                em.getTransaction().begin();
-//                em.merge(libro);
-//                em.getTransaction().commit();
-//                em.getTransaction().begin();
-//                em.persist(prestamo);
-//                em.getTransaction().commit();
-//            } else {
-//                throw new Exception("Error: no se puede realizar el prestámo ya que no hay ejemplares disponibles");
-//            }
-//        } catch (IllegalArgumentException e) {
-//            throw new Exception("Error: La fecha debe estar en el formato YYYY-MM-DD");
-//        } catch (Exception e) {
-//            throw e;
-//        }
-//    }
 //
 //    public void modificar(Scanner scanner, EntityManager em) throws Exception {
 //        Prestamo prestamo = new Prestamo();
 //    }
 //
-//    public boolean validacionPrestamo(Prestamo prestamo, EntityManager em) {
-//        // Define a JPQL query to check for the existence of a Prestamo with the same attributes
-//        String jpql = "SELECT p FROM Prestamo p WHERE p.fechaPrestamo = :fechaPrestamo " +
-//                "AND p.fechaDevolucion = :fechaDevolucion " +
-//                "AND p.libro = :libro " +
-//                "AND p.cliente = :cliente";
-//
-//        // Create a TypedQuery from the JPQL query
-//        TypedQuery<Prestamo> query = em.createQuery(jpql, Prestamo.class);
-//
-//        // Set parameters for the query
-//        query.setParameter("fechaPrestamo", prestamo.getFechaPrestamo());
-//        query.setParameter("fechaDevolucion", prestamo.getFechaDevolucion());
-//        query.setParameter("libro", prestamo.getLibro());
-//        query.setParameter("cliente", prestamo.getCliente());
-//
-//        // Execute the query and retrieve the results
-//        List<Prestamo> resultList = query.getResultList();
-//
-//        // Check if any matching Prestamo objects exist
-//        return !resultList.isEmpty();
-//    }
+
 }
